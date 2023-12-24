1e42cf3c0be13227578ecdfd8a718b93195406c9
sasipong jiteufeu <115165630+sasipongjiteufeu@users.noreply.github.com>
2023-12-25 02:48:15 +0700

    Add files via upload
    update v.1

---
+++ calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace calculator{
+    class Program{
+        static void Main(string[] args){
+            int num1 = 0,num2 = 0,num3 = 0;
+            
+            
+            Console.WriteLine("pick one");
+            Console.WriteLine("\n1) + \n 2) - \n 3) * \n 4) / \n 5) % \n");
+            string menu = Console.ReadLine();
+            Console.WriteLine("Your pick is " + menu);
+            Console.WriteLine("input your number_1\n");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input your number_2\n");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            
+            calculator_Logic myObj = new calculator_Logic();
+
+            if(menu == "+"){
+            myObj.sum(num1,num2);
+            Console.WriteLine(myObj.answer);
+            }
+            else if (menu == "-"){
+            myObj.subtract(num1,num2);
+            Console.WriteLine(myObj.answer); 
+            }
+            else if (menu == "*"){
+            myObj.multiply(num1,num2);
+            Console.WriteLine(myObj.answer);
+            }
+            else if (menu == "/"){
+            myObj.divide(num1,num2);
+            Console.WriteLine(myObj.answer);
+            }
+            else if (menu == "%"){
+            myObj.Modulus(num1,num2);
+            Console.WriteLine(myObj.answer);
+            }
+
+
+
+        } 
+    }
+}
+
---
+++ calculator/chack_input_op.cs
@@ -0,0 +1,47 @@
+using System;
+
+
+public class calculator_Logic {
+  public int answer;
+  public int sum(int num1,int num2)   
+    {
+
+    answer = num1 + num2;
+
+    return answer;
+
+    }
+    public int subtract(int num1,int num2)   
+    {
+
+    answer = num1 - num2;
+
+    return answer;
+
+    }
+    public int multiply(int num1,int num2)   
+    {
+
+    answer = num1 * num2;
+
+    return answer;
+
+    }
+    public int divide(int num1,int num2)   
+    {
+
+    answer = num1 / num2;
+
+    return answer;
+
+    }
+    public int Modulus(int num1,int num2)   
+    {
+
+    answer = num1 % num2;
+
+    return answer;
+
+    }
+}
+
